96ffda1084b13e1300ac23cd3786cc3afba3f730
MoonSungHoon <msh1342@naver.com>
2018-10-29 01:49:23 +0900

    asd

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,10 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
-    path(r'^post/new/$', views.post_new, name='post_new'),
-    path(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
+    path('post/(<pk>\d+)/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/(<pk>\d+)/edit/', views.post_edit, name='post_edit'),
+    path('drafts/', views.post_draft_list, name='post_draft_list'),
+    path('^post/(<pk>\d+)/publish/', views.post_publish, name='post_publish'),
+    path('^post/(<pk>\d+)/remove/', views.post_remove, name='post_remove'),
 ]
--- mysite/settings.py
+++ mysite/settings.py
@@ -120,3 +120,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
+# login
+LOGIN_REDIRECT_URL = '/'
+
+# logout
+LOGOUT_REDIRECT_URL = '/'
